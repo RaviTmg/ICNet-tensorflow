0c57b9715a9b8e540481d806fa60995b034fd217
Ravi Tmg <tmgrawv@gmail.com>
2018-12-03 10:36:42 +0545

    Update download_ADE20k.sh

--- script/download_ADE20k.sh
+++ script/download_ADE20k.sh
@@ -1,4 +1,4 @@
-wget -O ./data/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
-unzip ./data/ADEChallengeData2016.zip -d ./data
-rm ./data/ADEChallengeData2016.zip
+wget -O ICNet-tensorflow/data/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+unzip ICNet-tensorflow/data/ADEChallengeData2016.zip -d ./data
+rm ICNet-tensorflow/data/ADEChallengeData2016.zip
 echo "Dataset downloaded."
